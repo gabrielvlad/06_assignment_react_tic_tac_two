64f616a2c707e29a8ad15b6ecd3e6a998ea4d15f
gabrielvlad <gabriel.vlad@outlook.dk>
2022-10-09 20:31:25 +0200

    Rewritten Exercises 7 into 7.1 because I have realized and learned that I can write it in a different way
    Rewritten Exercises 7 into 7.1 because I have realized and learned that I can write it in a different way as-well inside the Program.cs file and will still work.

--- IT-DNP1X-A22/Exercises/Session1/OptionalExercises/S1_Ex7/Program.cs
+++ IT-DNP1X-A22/Exercises/Session1/OptionalExercises/S1_Ex7/Program.cs
@@ -10,22 +10,15 @@ makeAbba("What", "Up") → "WhatUpUpWhat"
 You can use the usual string concatenation from Java, but consider a C# specific approach.
 Call your method from the Main method a couple of times with various input and verify the output.
 */
-namespace S1_Ex7;
-/// <summary>
-/// 
-/// </summary>
-static class Program
+//As mentioned in S1_Ex7.1 comment I have realised and learned, that the program can be written 
+//without the "namespace namespace S1_Ex7;" and "class Program" wrappers around the prints
+//as the Program.cs file accepts printing the console like this as-well:
+Console.WriteLine(MakeAbba("Hi", "Bye"));
+Console.WriteLine(MakeAbba("Yo", "Alice"));
+Console.WriteLine(MakeAbba("What", "Up"));
+Console.WriteLine(MakeAbba("A", "B"));
+
+string MakeAbba(string a, string b)
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine(MakeAbba("Hi","Bye"));
-        Console.WriteLine(MakeAbba("Yo", "Alice"));
-        Console.WriteLine(MakeAbba("What", "Up"));
-        Console.WriteLine(MakeAbba("A","B"));
-        
-        string MakeAbba(string a, string b)
-        {
-            return $"{a}{b}{b}{a}";
-        }
-    }
+    return $"{a}{b}{b}{a}";
 }
