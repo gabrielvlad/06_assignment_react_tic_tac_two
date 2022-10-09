ec040e4a38dc791fcfcd4105a2a39774bdb5cd72
gabrielvlad <gabriel.vlad@outlook.dk>
2022-10-09 18:12:42 +0200

    Exercise S1_Ex7 from Optional ones

---
+++ IT-DNP1X-A22/Session_1_Exercises/DNPStructureTest/Session1/OptionalExercises/S1_Ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Create a new project. Create a method with the following behaviour:
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+Examples:
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+
+You can use the usual string concatenation from Java, but consider a C# specific approach.
+Call your method from the Main method a couple of times with various input and verify the output.
+*/
+namespace S1_Ex7;
+/// <summary>
+/// 
+/// </summary>
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(MakeAbba("Hi","Bye"));
+        Console.WriteLine(MakeAbba("Yo", "Alice"));
+        Console.WriteLine(MakeAbba("What", "Up"));
+        Console.WriteLine(MakeAbba("A","B"));
+        
+        string MakeAbba(string a, string b)
+        {
+            return $"{a}{b}{b}{a}";
+        }
+    }
+}
